cd31172f09bd1bf08e246303f9301394db5c44b6
gee06robson <robsom06@hotmail.com>
2022-02-14 07:37:07 -0300

    update responsive page.login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,7 +2,7 @@ import styles from "./styles.module.scss"
 import logo from "../../assets/logo.svg"
 import { useContext, useEffect } from "react"
 import { AuthContext } from "../../context/auth"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import useScript from "../../GoogleOneTap/useScript"
 
 export const Login = () => {
@@ -11,8 +11,8 @@ export const Login = () => {
   const googleClientScriptURL = VITE_GOOGLE_CLIENT_SCRIPT_URL as string
   const scriptFlag = VITE_SCRIPT_FLAG as string
   const script = useScript(googleClientScriptURL)
-  console.log(VITE_GOOGLE_CLIENT_ID, VITE_GOOGLE_CLIENT_SCRIPT_URL, VITE_SCRIPT_FLAG)
-  console.log(import.meta.env)
+  const navigate = useNavigate()
+
   const checkUser = (obj: object | null) => {
     return typeof obj === 'object' && obj !== null
   }
@@ -46,6 +46,12 @@ export const Login = () => {
 
   }, [script, window?.[scriptFlag]])
 
+  useEffect(() => {
+    if(checkUser(user)===true) {
+      navigate("/financial/home")
+    }
+  }, [checkUser(user)])
+
   return (
     <div className={styles.loginBoxWrapper}>
       <div className={styles.loginContentBox}>
@@ -55,17 +61,12 @@ export const Login = () => {
 
           <div className={styles.contentTitle}>
             <span>Batalhão de Polícia do Exército de Brasília</span>
-            <span>Sistema de Gêrenciamento de Notas Fiscais e Faturas</span>
+            <span className={styles.textWeb} >Sistema de Gêrenciamento de Notas Fiscais e Faturas</span>
+            <span className={styles.textMobile} >Setor Financeiro</span>
           </div>
 
           <div className={styles.contentButtonLogin}>
-          {checkUser(user) 
-          
-          ? <Link to="/financial/home"> 
-              <span>{`Seja bem vindo ${user?.name}`}</span>
-            </Link>
-
-          : <div id="buttonDiv" /> }
+            { checkUser(user)===false && <div id="buttonDiv" /> }
           </div>
 
         </div>
